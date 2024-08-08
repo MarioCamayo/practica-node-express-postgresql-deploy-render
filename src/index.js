54c1dd2c8fd985c303f5338c329d33133dfe414c
mario camayo <mariocamayo17@gmail.com>
2024-08-07 20:02:18 -0500

    se agregó la ruta de premiun

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ app.get('/products', (req, res)=>{
 app.get('/pay', (req, res)=>{
   res.send('<h1>ya puedes pagar tus productos</h1>')
 })
+app.get('/permiun', (req, res)=>{
+  res.send('<h1>Tienes Acceso Ilimitado a la Aplicación</h1>')
+})
 app.listen(3001, ()=>{
   console.log('server listening on port 3001')
 })
