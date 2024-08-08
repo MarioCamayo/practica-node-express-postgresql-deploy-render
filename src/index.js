522aeae11a32c061ad241fd736500ca85f7b115f
mario camayo <mariocamayo17@gmail.com>
2024-08-07 19:39:45 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res)=>{
+  res.send('<h1>Hello welcome</h1>')
+})
+app.get('/products', (req, res)=>{
+  res.send('<h1>Estos son tus productos</h1>')
+})
+app.get('/pay', (req, res)=>{
+  res.send('<h1>ya puedes pagar tus productos</h1>')
+})
+app.listen(3001, ()=>{
+  console.log('server listening on port 3001')
+})
